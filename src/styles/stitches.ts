9c09f6fdd68e32584d17cef6c7d33ea882062e11
safethecode <iam@sonjim.in>
2023-03-14 03:34:31 +0900

    feat(styles/stitches): fontWeights 속성 추가

--- src/styles/stitches.ts
+++ src/styles/stitches.ts
@@ -134,6 +134,17 @@ export const {
       inverseGrey800: '#e4e4e5',
       inverseGrey900: '#fff',
     },
+    fontWeights: {
+      1: '100',
+      2: '200',
+      3: '300',
+      4: '400',
+      5: '500',
+      6: '600',
+      7: '700',
+      8: '800',
+      9: '900',
+    },
     fontSizes: {
       1: '4px',
       2: '8px',
