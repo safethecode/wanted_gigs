50dbf566284f5f47a8424353e4811d83802dad3d
safethecode <iam@sonjim.in>
2023-03-18 16:11:05 +0900

    feat(constants/telegramMessages): 새로운 외주 등록 및 매칭 요청 메시지 템플릿 추가

---
+++ src/constants/telegramMessages.ts
@@ -0,0 +1,27 @@
+import { Row } from 'typings/wantedGigs';
+
+export const registMessage = (message: Row) => `
+<b>💌 새로운 외주가 등록되었습니다.</b>
+%0A%0A
+<b>제목</b>: [${message.id}] ${message.title} <b>(${message.work_place_txt})</b>
+%0A
+<b>분류</b>: ${message.jobs}
+%0A%0A
+<b>급여</b>: 최소 ${message.salary.start}만원 ~ 최고 ${message.salary.end}만원
+%0A%0A
+<b>근무기간</b>: ${message.term.start}개월 ~ ${
+  message.term.end ? message.term.end + '개월' : ''
+}
+%0A
+<b>근무장소</b>: ${message.address}
+`;
+
+export const registSuccessMessage = (message: Row) => `
+<b>🎉 매칭 요청이 완료되었습니다.</b>
+%0A%0A
+<b>제목</b>: [${message.id}] ${message.title} <b>(${message.work_place_txt})</b>
+%0A
+<b>요청 일자</b>: ${new Date().toLocaleDateString()}
+%0A%0A
+<b>상세보기</b>: https://www.wanted.co.kr/gigs/projects/${message.id}
+`;
